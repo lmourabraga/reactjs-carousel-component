441af198a20ec873a3f8187a613a72a170ea610a
Luã Braga <ldemourabraga@gmail.com>
2021-04-14 02:30:16 -0300

    chore: improving background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { Carousel } from './components/Carousel';
 import { ItemCarousel } from './components/Carousel/ItemCarousel';
 
+import space1 from '../src/assets/images/space-1.jpeg';
+import space2 from '../src/assets/images/space-2.jpeg';
+import space3 from '../src/assets/images/space-3.jpeg';
+import space4 from '../src/assets/images/space-4.jpeg';
+import space5 from '../src/assets/images/space-5.jpeg';
+import space6 from '../src/assets/images/space-6.jpeg';
+
 import './styles/global.scss';
 
 export function App() {
@@ -15,9 +22,9 @@ export function App() {
             <div className="row">
                 <Carousel>
                     <ItemCarousel
-                        backgroundImage="https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2623&q=80"
+                        backgroundImage={space1}
                     >
-                        <h1>Case study 0: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
+                        <h1>Slide 1: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
                         <p>Read how Puma Mexico became the first among Puma international eCom stores to adopt ScandiPWA, becoming the fastest among the company’s websites.</p>
                         <a href="https://scandiweb.com/blog/case-study-puma-mexico-becomes-the-fastest-puma-store-after-adopting-scandipwa/">
                             Read more
@@ -25,9 +32,9 @@ export function App() {
                     </ItemCarousel>
 
                     <ItemCarousel
-                        backgroundImage="https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2623&q=80"
+                        backgroundImage={space2}
                     >
-                        <h1>Case study 1: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
+                        <h1>Slide 2: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
                         <p>Read how Puma Mexico became the first among Puma international eCom stores to adopt ScandiPWA, becoming the fastest among the company’s websites.</p>
                         <a href="https://scandiweb.com/blog/case-study-puma-mexico-becomes-the-fastest-puma-store-after-adopting-scandipwa/">
                             Read more
@@ -35,9 +42,9 @@ export function App() {
                     </ItemCarousel>
 
                     <ItemCarousel
-                        backgroundImage="https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2623&q=80"
+                        backgroundImage={space3}
                     >
-                        <h1>Case study 2: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
+                        <h1>Slide 3: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
                         <p>Read how Puma Mexico became the first among Puma international eCom stores to adopt ScandiPWA, becoming the fastest among the company’s websites.</p>
                         <a href="https://scandiweb.com/blog/case-study-puma-mexico-becomes-the-fastest-puma-store-after-adopting-scandipwa/">
                             Read more
@@ -45,27 +52,27 @@ export function App() {
                     </ItemCarousel>
 
                     <ItemCarousel
-                        backgroundImage="https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2623&q=80"
+                        backgroundImage={space4}
                     >
-                        <h1>Case study 3: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
+                        <h1>Slide 4: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
                         <p>Read how Puma Mexico became the first among Puma international eCom stores to adopt ScandiPWA, becoming the fastest among the company’s websites.</p>
                         <a href="https://scandiweb.com/blog/case-study-puma-mexico-becomes-the-fastest-puma-store-after-adopting-scandipwa/">
                             Read more
                             </a>
                     </ItemCarousel>
                     <ItemCarousel
-                        backgroundImage="https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2623&q=80"
+                        backgroundImage={space5}
                     >
-                        <h1>Case study 4: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
+                        <h1>Slide 5: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
                         <p>Read how Puma Mexico became the first among Puma international eCom stores to adopt ScandiPWA, becoming the fastest among the company’s websites.</p>
                         <a href="https://scandiweb.com/blog/case-study-puma-mexico-becomes-the-fastest-puma-store-after-adopting-scandipwa/">
                             Read more
                             </a>
                     </ItemCarousel>
                     <ItemCarousel
-                        backgroundImage="https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2623&q=80"
+                        backgroundImage={space6}
                     >
-                        <h1>Case study 5: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
+                        <h1>Slide 6: Puma Mexico Becomes the Fastest Puma Store After Adopting ScandiPWA</h1>
                         <p>Read how Puma Mexico became the first among Puma international eCom stores to adopt ScandiPWA, becoming the fastest among the company’s websites.</p>
                         <a href="https://scandiweb.com/blog/case-study-puma-mexico-becomes-the-fastest-puma-store-after-adopting-scandipwa/">
                             Read more
--- src/components/Carousel/ItemCarousel/styled.js
+++ src/components/Carousel/ItemCarousel/styled.js
@@ -10,6 +10,8 @@ export const Item = styled.div`
     width: 100%; 
     padding: 32px;
     text-align: center;
+    border: 1px solid #fff;
+    border-radius: 8px;
 
     ${props => props.backgroundImage && css`
         background: url(${props => props.backgroundImage}) rgba(0, 0, 0, 0.5);
